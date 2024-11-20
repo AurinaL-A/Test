1053bafb147eb6ae11080a3027364f4c7a8649a2
AurinaL-A <irina.shapochkina5432@gmail.com>
2024-11-21 02:33:32 +0500

    Бургерное меню

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnMenu = document.querySelector('.burger')
+const menuList = document.querySelector('.menu')
+
+btnMenu.addEventListener('click', ()=> {
+    menuList.classList.toggle('none')
+    // console.log('sds')
+})
