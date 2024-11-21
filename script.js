6c538e6bc0e2addaaeca06ee9daf05568aaa42c7
AurinaL-A <irina.shapochkina5432@gmail.com>
2024-11-21 21:20:20 +0500

    Удаление ненужного

--- script.js
+++
@@ -1,7 +0,0 @@
-const btnMenu = document.querySelector('.burger')
-const menuList = document.querySelector('.menu')
-
-btnMenu.addEventListener('click', ()=> {
-    menuList.classList.toggle('none')
-    // console.log('sds')
-})
